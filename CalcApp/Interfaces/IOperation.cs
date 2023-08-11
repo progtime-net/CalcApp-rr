841bf0a6407f9a9906d28fb0116448acf0d5edc4
linuxfight <kiko13024@gmail.com>
2023-08-11 15:22:53 +0300

    Added IOperation

---
+++ CalcApp/Interfaces/IOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcApp.Interfaces
+{
+    public interface ICalculate
+    {
+        static string? operation;
+        float Calculate(float value1, float value2);
+    }
+}
