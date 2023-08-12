38caa1371151ee4d006a5b3deb807ee9c25664bf
linuxfight <kiko13024@gmail.com>
2023-08-12 13:26:07 +0300

    operations1

--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -6,6 +6,40 @@
     }
 }
 
+class Add : ICalculator
+{
+    public float Operation(float a, float b)
+    {
+        return a + b;
+    }
+}
+
+class Div : ICalculator
+{
+    public float Operation(float a, float b)
+    {
+        return a / b;
+    }
+}
+
+class Percent : ICalculator
+{
+    public float Operation(float a, float b)
+    {
+        return a % b;
+    }
+}
+
+class Degree : ICalculator
+{
+    public float Operation(float a, float b)
+    {
+        if (b == 1)
+            return a;
+        return a * Operation(a, b - 1);
+    }
+}
+
 interface ICalculatorInput
 {
     string[] ParseInput(string input);
