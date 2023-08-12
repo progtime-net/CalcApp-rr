5b08f8a6639ddfea73b65b638a07e0d38437af0c
TheFirest-Artemis <artem.rvachev2@gmail.com>
2023-08-12 12:54:47 +0300

    commit artem to master

--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -6,44 +6,6 @@
     }
 }
 
-class CalcInput : ICalculatorInput
-{
-    string[] ICalculatorInput.ParseInput(string input)
-    {
-        return input.Split();
-    }
-
-    float ICalculatorInput.ReturnOperate(string input, float a, float b)
-    {
-        if (input == "+")
-        {
-            Plus plus = new Plus();
-            return plus.Operation(a, b);
-        }
-        else
-        {
-            Minus minus = new Minus();
-            return minus.Operation(a, b);
-        }
-    }
-}
-
-class Plus : ICalculator
-{
-    public float Operation(float a, float b)
-    {
-        return a + b;
-    }
-}
-
-class Minus : ICalculator
-{
-    public float Operation(float a, float b)
-    {
-        return a - b;
-    }
-}
-
 interface ICalculatorInput
 {
     string[] ParseInput(string input);
